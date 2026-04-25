61df3c25939ede25937e38dee14fed6e79d70008
Hasith89 <hasithakmeemanacse@gmail.com>
2026-04-25 16:11:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const accountingRoutes = require('./routes/accountingRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
