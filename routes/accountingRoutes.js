400a504ecc2a741b9f39ed209bf91d40340bcffb
Hasith89 <hasithakmeemanacse@gmail.com>
2026-05-02 10:20:17 +0530

    Update accountingRoutes.js

--- routes/accountingRoutes.js
+++ routes/accountingRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProfitLossReport } = require('../controllers/accountingController');
 const verifyToken = require('../middleware/authMiddleware');
 const allowRoles = require('../middleware/roleMiddleware');
 
+const {
+    getProfitLossReport,
+    getProductProfitReport
+} = require('../controllers/accountingController');
+
 router.get('/profit-loss', verifyToken, allowRoles('owner', 'admin'), getProfitLossReport);
+router.get('/product-profit', verifyToken, allowRoles('owner', 'admin'), getProductProfitReport);
 
 module.exports = router;
