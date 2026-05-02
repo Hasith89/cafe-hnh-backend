dd39692ad970542056f567a1594548e7ce14dd79
Hasith89 <hasithakmeemanacse@gmail.com>
2026-05-02 11:38:11 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const verifyToken = require('../middleware/authMiddleware');
+const allowRoles = require('../middleware/roleMiddleware');
+
 const {
     getProducts,
     createProduct,
@@ -8,12 +11,9 @@ const {
     deleteProduct
 } = require('../controllers/productController');
 
-const verifyToken = require('../middleware/authMiddleware');
-const allowRoles = require('../middleware/roleMiddleware');
-
 router.get('/', verifyToken, getProducts);
 router.post('/', verifyToken, allowRoles('owner', 'admin'), createProduct);
-router.put('/:id', verifyToken, allowRoles('owner'), updateProduct);
+router.put('/:id', verifyToken, allowRoles('owner', 'admin'), updateProduct);
 router.delete('/:id', verifyToken, allowRoles('owner'), deleteProduct);
 
 module.exports = router;
