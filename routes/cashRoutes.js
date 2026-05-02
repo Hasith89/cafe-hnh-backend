5a5f514d1de6ec470bd81d0eed6e9a419060fa12
Hasith89 <hasithakmeemanacse@gmail.com>
2026-05-02 10:16:48 +0530

    Update cashRoutes.js

--- routes/cashRoutes.js
+++ routes/cashRoutes.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const verifyToken = require('../middleware/authMiddleware');
+
 const {
     openCashSession,
     addCashIn,
     addCashOut,
-    closeSession
+    closeSession,
+    getCashReport
 } = require('../controllers/cashController');
 
 router.post('/open', verifyToken, openCashSession);
 router.post('/cash-in', verifyToken, addCashIn);
 router.post('/cash-out', verifyToken, addCashOut);
 router.post('/close', verifyToken, closeSession);
+router.get('/report', verifyToken, getCashReport);
 
 module.exports = router;
